de5eccd1f0b5ef03649b77d5cd444872e5dc47a4
Gialandro <gialandros@gmail.com>
2018-04-21 02:39:41 -0500

    Iniciando menu

--- littleATM.py
+++ littleATM.py
@@ -8,11 +8,31 @@
 #Y si se puede cada que ingrese a una opción o cabie de login a menu se limpie la pantalla
 
 import getpass
+import os
 
-us = 'Admin'
-pwd = 'p@$$w0rd'
+us = '123'#'Admin'
+pwd = '123'#'p@$$w0rd'
 username = ''
 password = ''
-while username != us and pwd != password:
+menu = 0
+capital = 9999.99
+ingreso = 0.00
+retiro = 7000.00
+while username != us or pwd != password:
+    os.system('cls || clear')
+    if username != '' and username != us:
+        print('Usuario incorrecto, intente nuevamente.')
+    if password != '' and password != pwd:
+        print('Contraseña incorrecta, intente nuevamente.')
+    if username == '' and password == '':
+        print('***Bienvenido*** \n Ingrese sus datos para acceder...')
     username = input('Usuario: ')
     password = getpass.getpass('Contraseña: ')
+    pass
+if username == us and password == pwd:
+    while menu <= 0 or menu >= 4:
+        menu = int(input('Menu: \n 1. Ingresar \n 2. Retirar \n 3. Consultar \n 4. Salir \n'))
+        if menu == 1:
+            while ingreso >= 7000.00:
+                ingreso = float(input('Cantidad a depositar (Maximo $7000.00): $'))
+            pass
